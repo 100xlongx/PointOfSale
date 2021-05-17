b529bc880478318cd1845d11d42a0ce7e9e43e74
Raaed Khan <raaedk@gmail.com>
2021-05-17 11:07:14 -0400

    init product class

---
+++ Product.cs
@@ -0,0 +1,11 @@
+
+namespace PointOfSale
+{
+    class Product
+    {
+        public string name { get; set; }
+        public int pricing { get; set; }
+
+        public int quantity { get; set; }
+    }
+}
